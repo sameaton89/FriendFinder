6b32196cfb799ae80d5779fce452d0556a429d6c
Sam Eaton <sameaton1989@gmail.com>
2018-11-28 19:26:34 -0500

    adding edited survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,57 @@ var friends = [
         4,
         1
       ]
+    },
+    {
+        name: "Leslie",
+        photo: "https://www.abc.net.au/news/image/7554070-3x2-700x467.jpg",
+        phoneNumber: "555-555-0493",
+        scores:[
+          3,
+          2,
+          5,
+          3,
+          2,
+          1,
+          5,
+          3,
+          2,
+          1
+        ]
+      },
+      {
+        name: "Yassiel",
+        photo: "https://media.mutualart.com/Images/2018_03/13/19/192220394/97061c80-c583-430c-afb5-0f6b41fc13e7_570.Jpeg",
+        phoneNumber: "555-555-5555",
+        scores:[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+      },
+      {
+      name: "Jack Dracula",
+      photo: "https://news.artnet.com/app/news-upload/2016/07/2.-Jack-Dracula-at-a-bar-New-London-Conn.-1961-e1468521305829.jpg",
+      phoneNumber: "555-555-4984",
+      scores: [
+          4,
+          3,
+          2,
+          1,
+          5,
+          2,
+          1,
+          5,
+          4,
+          3
+      ]
     }
   ];
 
